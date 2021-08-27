2daf930714655b58c211d1a64bd40c48460538fa
VorgSpring <drmrSP@yandex.ru>
2021-08-27 13:26:41 +0300

    fix build

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import React from 'react';
-import { store, history} from './store';
-
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
+import { store, history } from './store';
 
 import App from './components/App';
 
