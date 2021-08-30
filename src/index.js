5405b100f2dc9074951310b62a099efa79be7cc4
Kisterev Aleksei <akisterev@list.ru>
2021-08-30 12:44:27 +0600

    update redux and react-router-redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import { store, history } from './store';
 
 import App from './components/App/App';
