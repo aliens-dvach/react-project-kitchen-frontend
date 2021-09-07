91460601c8328ff5528f3e086218b7269bcb2d26
Kisterev Aleksei <akisterev@list.ru>
2021-09-07 15:17:04 +0600

    fix: add new version react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { store, history } from './store';
 
@@ -11,11 +11,13 @@ import App from './components/App/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
+    <Router>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </ConnectedRouter>
+    </Router>
   </Provider>,
 
   document.getElementById('root'),
